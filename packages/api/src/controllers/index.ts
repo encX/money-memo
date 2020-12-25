fca903e7f075590d118a0dff8df901996c44f7e5
Plai <plai.rt.w@gmail.com>
2020-12-25 15:21:46 +0700

    Add individual controllers for asset record and type

--- packages/api/src/controllers/index.ts
+++ packages/api/src/controllers/index.ts
@@ -1,3 +1,5 @@
 export * from './asset-type-asset-record.controller';
 export * from './user-asset-type.controller';
 export * from './user.controller';
+export * from './asset-record.controller';
+export * from './asset-type.controller';
