4db20ff93b0a9e731e21319c085392e9439c4e04
Plai <plai.rt.w@gmail.com>
2020-12-25 15:12:42 +0700

    Remove unnecessary controllers

--- packages/api/src/controllers/index.ts
+++ packages/api/src/controllers/index.ts
@@ -1,5 +1,3 @@
 export * from './asset-type-asset-record.controller';
-export * from './asset-record-asset-type.controller';
 export * from './user-asset-type.controller';
-export * from './asset-type-user.controller';
 export * from './user.controller';
